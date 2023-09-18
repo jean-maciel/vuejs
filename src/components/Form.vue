fb9137d504b427df8524754266b79fb911507de6
jean.reis <jeanlucas.reis@gmail.com>
2023-09-18 16:27:56 -0300

    VueJS studies

---
+++ src/components/Form.vue
@@ -0,0 +1,56 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "newForm",
+  data() {
+    return { time: 0, stopwatch: 0 };
+  },
+  computed: {
+    passedTime(): string {
+      return new Date(this.time * 1000).toISOString().substring(11, 19);
+    },
+  },
+  methods: {
+    start() {
+      this.stopwatch = setInterval(() => this.time++, 1000);
+    },
+    stop() {
+      clearInterval(this.stopwatch);
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="box">
+    <div class="columns">
+      <div aria-label="New task form" class="column is-8" role="form">
+        <input class="input" placeholder="Task" type="text" />
+      </div>
+      <div class="column">
+        <div
+          class="is-flex is-align-items-center is-justify-content-space-between"
+        >
+          <section>
+            <strong> {{ passedTime }}</strong>
+          </section>
+          <button class="button" @click="start">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+          </button>
+          <button class="button" @click="stop">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SideBar",
+});
+</script>
+
+<template>
+  <header>
+    <h1>
+      <img alt="" src="../assets/logo.png" />
+    </h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  padding: 1rem;
+  background: aquamarine;
+  width: 100%;
+  height: 100vh;
+}
+
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
